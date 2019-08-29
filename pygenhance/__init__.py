c3f4cfa293fdfc7e392a25760664aea5e2a3af46
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2019-08-29 15:09:41 -0500

    8/29/2019

--- pygenhance/__init__.py
+++ pygenhance/__init__.py
@@ -1,7 +1,8 @@
-__doc__ = """Here is an example:
-    ```python
-    %s
-    ```""" % (
+"""Pygenhance is short for Pygame Enhance. Pygenhance exports a Unity-like interface for making
+games. It uses Pygame internally."""
+
+__doc__ += """\nHere is an example:
+    %s""" % (
     open(__file__[:-11] + '__main__.py').read().replace('\n', '\n    ')
 )
 __version__ = '0.0.1'
@@ -12,16 +13,55 @@ from pygame import *
 import math
 from gameobjects.vector2 import Vector2
 
-class ObjectDisposedError(RuntimeError): pass
+class ObjectDisposedError(RuntimeError):
+    """This error occurs when an object is run through and done for (e.g. this error occurrs when a
+Game object's mainloop is called after it has already been run)"""
+    pass
 from . import time
 
 class Game:
-    def __init__(self, screenpw=(), screenkw={}, frame_rate=None):
+    """This is the container object for running games.
+
+Parameters
+----------
+screenpw : iterable
+    the postional arguments to pass into Pygame's display set_mode function; a tuple as the
+    resolution must be supplied as the first argument, otherwise it defaults to 640x480; if the 
+    FULLSCREEN constant is passed as the second argument, the game will run in fullscreen
+
+    examples:
+        ``[(640, 480)]``
+        ``[(640, 480), FULLSCREEN]``
+screenkw : dict
+    the keyword arguments to pass into Pygame's display set_mode function
+frame_rate : number or None
+    if set to None or left alone, it will default to the highest fps your computer can handle"""
+    def __init__(self, screenpw=[(640, 480)], screenkw={}, frame_rate=None):
         pygame.init()
         self.screendata = screenpw, screenkw
         self._closed = False
         self.frame_rate = frame_rate
     def mainloop(self, scene):
+        """Runs the game's mainloop (more commonly known as gameloop)
+
+Parameters
+----------
+scene : Scene
+    the scene to start with when the game is run
+
+Raises
+------
+ObjectDisposedError
+    if game has been run already and is trying to be run again
+
+Warnings
+--------
+the game can only be run once because Pygame deinitializes after the mainloop is run once; this can
+be bypassed with the following lines::
+    import pygame
+    game._closed = False
+    pygame.init()
+    game.mainloop(scene)"""
         if self._closed:
             raise ObjectDisposedError('Game has already been run and cannot be run again.')
         self.screen = pygame.display.set_mode(*self.screendata[0], **self.screendata[1])
@@ -58,9 +98,28 @@ class Game:
                 for component in obj.components:
                     yield component
     def close(self):
+        """Ends the mainloop and exits the game"""
         self._closed = True
 
 class GameObject:
+    """Represents an object in a scene
+
+Attributes
+----------
+parent : GameObject or Scene
+    the parent object of this object
+children : list
+    the list of this object's children
+components : list
+    the list of this object's component; the component in index 0 will always be a Transform
+    component
+source : str
+    a string describing where this object came from
+    
+Parameters
+----------
+parent : GameObject or Scene
+    the parent object of this object"""
     def __init__(self, parent):
         self.source = '@%x' % id(self)
 
@@ -72,6 +131,7 @@ class GameObject:
     def __repr__(self):
         return '<GameObject in=%r from=%r>' % (self.parent, self.source)
     def recur_children(self):
+        """Recurs through all the children of this object"""
         for child in self.children:
             yield child
             for sub_child in child.recur_children():
@@ -79,6 +139,12 @@ class GameObject:
     def _add_child(self, child):
         self.children.append(child)
     def dispose(self, recursive=False):
+        """Deletes all references to other objects so thay can be easily garbage collected
+
+Parameters
+----------
+recursive : bool
+    if True, all children will also be disposed"""
         self.parent = None
         if recursive:
             for child in self.recur_children():
@@ -87,7 +153,19 @@ class GameObject:
         self.components = []
     def __del__(self): self.dispose(True)
     def add_component(self, cls):
-        if cls is Transform: raise TypeError
+        """Adds a new component to the object created by the specified callable; sprite functions
+like create_color_sprite can be called when passed into this function (e.g.
+``obj.add_component(create_color_sprite(size=(100, 100)))``)
+
+Parameters
+----------
+cls
+
+Raises
+------
+TypeError
+    if you attempt to add an additional Transform component"""
+        if cls is Transform: raise TypeError('Attempted to add an additional Transform component')
         self.components.append(cls(self))
     def has_component(self, cls):
         for c in self.components:
@@ -163,6 +241,7 @@ class Scene:
     def _add_child(self, child):
         if not hasattr(self, 'ROOT'):
             self.ROOT = child
+            self.ROOT.components = []
         else:
             self.ROOT._add_child(child)
     def recur_children(self):
@@ -176,9 +255,12 @@ class Scene:
             self.ROOT.add_component(debug.Debugger)
         return self.ROOT.get_component(debug.Debugger)
     def dispose(self, recursive=1):
-        if recursive:
+        has_root = hasattr(self, 'ROOT')
+        if recursive and has_root:
             self.ROOT.dispose(recursive-1)
-        del self.ROOT
+        self.game = None
+        if has_root: del self.ROOT
+    def __del__(self): self.dispose(2)
 
 _globals = dir()
 _pygame = dir(sys.modules['pygame'])
@@ -188,4 +270,7 @@ for x in _globals:
         if x not in _pygame:
             __all__.append(x)
 del x
+__all__.append('FULLSCREEN')
+__all__.append('time')
+__all__.append('Vector2')
 # __all__ = [x for x in _globals if not x in _pygame or x[0] != '_']
--- pygenhance/__main__.py
+++ pygenhance/__main__.py
@@ -1,22 +1,22 @@
-import pygenhance
-# import pygenhance.debug as debug
-import pygenhance.time as time
-game = pygenhance.Game([(640, 480)])
-scene = pygenhance.Scene()
+from pygenhance import *
+# import debug as debug
+# game = Game([(640, 480), FULLSCREEN])
+game = Game([(640, 480)])
+scene = Scene()
 # deb = scene.get_debugger()
 #scene.get_debugger().debuggers.append(debug.Delta_Time)
 #scene.get_debugger().debuggers.append(debug.Frame_Rate)
-obj = pygenhance.GameObject(scene)
-# surf = pygenhance.Surface((250, 250))
+obj = GameObject(scene)
+# surf = Surface((250, 250))
 # surf.fill((255, 255, 255))
-# comp = pygenhance.Sprite(obj, surf)
+# comp = Sprite(obj, surf)
 # obj.components.append(comp)
-# obj.add_component(pygenhance.make_color_sprite(size=(250, 250)))
-# obj.add_component(pygenhance.make_text_sprite(text="Josiah's Dad loves his family", font='Corbel', fontsize=25))
-obj.add_component(pygenhance.make_image_sprite(r"c:\users\josia\pictures\dat dat dat.png", size=(153, 55)))
-width = obj.get_component(pygenhance.Sprite).surface.get_width()
-obj.transform.position = pygenhance.Vector2(100, 100)
-class test(pygenhance.Behaviour):
+# obj.add_component(make_color_sprite(size=(250, 250)))
+# obj.add_component(make_text_sprite(text="Josiah's Dad loves his family", font='Corbel', fontsize=25))
+obj.add_component(make_image_sprite(r"c:\users\josia\pictures\dat dat dat.png", size=(153, 55)))
+width = obj.get_component(Sprite).surface.get_width()
+obj.transform.position = Vector2(100, 100)
+class test(Behaviour):
     def update(self):
         self.transform.position.x += 50 * time.deltatime
         if self.transform.position.x > 640:
@@ -24,3 +24,4 @@ class test(pygenhance.Behaviour):
         print(self.transform.position, end='\r')
 obj.add_component(test)
 game.mainloop(scene)
+scene.dispose(2)
